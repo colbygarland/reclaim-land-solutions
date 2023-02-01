4573b365f86a0bc760aee7287f2cd5b1cec624b5
Colby Garland <colby.garland@hootsuite.com>
2023-01-31 19:55:41 -0700

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import type { AppProps } from 'next/app';
 import { StoreProvider, rootStore } from '../stores/rootStore';
 import 'tailwindcss/tailwind.css';
 import { PageHead } from '../components/PageHead';
-
+//
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <StoreProvider value={rootStore}>
