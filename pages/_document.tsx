35909d01b45c70656d3f9f76dc7e802deecf47f6
Colby Garland <colbygarland@gmail.com>
2024-02-29 10:07:01 -0700

    add missing window call

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,7 +11,7 @@ export default function Document() {
           rel="stylesheet"
         />
         {/* @ts-ignore */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-7E8B8XXYBR"></script> <script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments)} gtag('js', new Date()); gtag('config', 'G-7E8B8XXYBR'); </script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-7E8B8XXYBR"></script> <script> window.dataLayer = window.dataLayer || []; function gtag(){window.dataLayer.push(arguments)} gtag('js', new Date()); gtag('config', 'G-7E8B8XXYBR'); </script>
       </Head>
       <body>
         <Main />
