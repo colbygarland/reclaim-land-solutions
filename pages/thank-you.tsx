8443eab7735c7fe305fb627776867b1ff8b60b62
Colby Garland <colbygarland@gmail.com>
2024-03-26 21:09:21 -0600

    add phone num; bigger images

--- pages/thank-you.tsx
+++ pages/thank-you.tsx
@@ -1,11 +1,12 @@
 import { Hero } from '../components/Hero'
+import { PHONE_NUMBER } from '../constants'
 
 export default function ThankYou() {
   return (
     <>
       <Hero
         title="Thank You"
-        description="We have received your message. We will get back to you as soon as possible."
+        description={`We will be in touch as soon as we are available to. If you wish to speak to somebody right now, please call ${PHONE_NUMBER}`}
         media="/placeholder-image-2.jpeg"
         condensed
       />
